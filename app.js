d4f8dd6211ac6a8610b9222aa2fff4026e132d68
Admin <admin@Admins-MacBook-Pro.local>
2016-08-04 21:36:22 -0700

    more stuff

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 $('button').on('click', function() {
         var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-        url += '?' + $.param({
+        var obj = {
           'api-key': "84d21462dd9242caaa0347095749defd",
           'q': "Elon Musk",
           'fq': "5",
           'begin_date': "2000",
           'end_date': "2016"
-        });
+        };
+        obj.q = "George W Bush";
+        obj.fq = 1;
+        obj.begin_date = "2000";
+        obj.end_date = "2016";
+        url += '?' + $.param(obj);
+        console.log(url);
         $.ajax({
           url: url,
           method: 'GET',
